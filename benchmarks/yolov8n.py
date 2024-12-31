52575efcf11e4c05360c9ae96854d19ee1a5955b
Alvise Susmel <alvise@poeticoding.com>
2024-12-31 17:32:37 +0100

    updated python benchmark

---
+++ benchmarks/yolov8n.py
@@ -0,0 +1,35 @@
+from ultralytics import YOLO
+import time
+
+model = YOLO("models/yolov8n.pt")
+
+total = 0
+count = 0
+min_time = 10
+max_time = -1
+
+# warmup
+for i in range(1, 10):
+  model(["benchmarks/images/traffic.jpg"], device='cpu')
+  # Mac
+  # model(["benchmarks/images/traffic.jpg"], device='mps')
+  
+  # CUDA
+  # model(["benchmarks/images/traffic.jpg"], device='cuda')
+
+for i in range(1, 10):
+  start = time.time()
+  model(["benchmarks/images/traffic.jpg"])
+  stop = time.time()
+  diff = stop - start
+  count += 1
+  total += diff
+  if min_time > diff:
+    min_time = diff
+  if max_time < diff:
+    max_time = diff
+
+print("count: ", count)
+print("avg: ", total/count)
+print("min: ", min_time)
+print("max: ", max_time)
