e17aafb7664a7222eeb24ded5ed78f8a33e34d20
Alvise Susmel <alvise@poeticoding.com>
2025-06-19 17:30:39 +0200

    added moduledoc

--- lib/yolo.ex
+++ lib/yolo.ex
@@ -1,4 +1,9 @@
 defmodule YOLO do
+  @moduledoc """
+  This module provides the main entry point and library context for YOLO object detection in Elixir.
+  It delegates to the underlying model and utility modules for most functionality.
+  """
+
   defdelegate load(options), to: YOLO.Models
   defdelegate detect(model, image), to: YOLO.Models
   defdelegate detect(model, image, options), to: YOLO.Models
